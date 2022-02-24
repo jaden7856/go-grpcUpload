6a25d208a46743312e8c4984de5fc5afee96c1c9
Jaden Jo <jhg7856@gmail.com>
2022-02-24 11:13:19 +0900

    run server script file

---
+++ AsyngRPC/runserver.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+gsTarget=( "AsyngRPCs" )
+gsCL=( "cnt" "lop" )
+gnPort=50050
+
+pkill -9 go-server
+
+for sTarget in ${gsTarget[*]}
+do
+	echo ${sTarget}
+	cd ${sTarget}
+
+	if [ "$?" == "0" ]
+	then
+			for nSize in 512 # 1024 2048 4096 8192 16384 32768 65536
+			do
+				echo "./server/go-server-$sTarget -add=centos7ora2:$gnPort -size=$nSize &"
+				./server/go-server-$sTarget -add=centos7ora2:$gnPort -size=$nSize &
+
+				if [ "$?" == "0" ]
+				then
+					echo "OK"	
+				else
+					echo "Failure"
+				fi
+			done
+	else
+		echo "Failure"
+	fi
+
+	cd ..
+	echo $(pwd)
+done
