edd0e89597afa42f7def9da9132cd879e70b0b23
Jaden Jo <jhg7856@gmail.com>
2022-03-07 07:57:10 +0900

    AsynSocket 속도 테스트 server

--- AsyngRPC/AsynSocket/server/server.go
+++ AsyngRPC/AsynSocket/server/server.go
@@ -15,11 +15,9 @@ ex) exe -add=192.168.124.131:50056 -tls=tls -size=512 -debug=0
 package main
 
 import (
-	"crypto/tls"
 	"flag"
 	"fmt"
 	"net"
-	"sync"
 )
 
 const BUFSIZE = 8388608  // 1024 * 1024 * 8
@@ -32,17 +30,15 @@ func main() {
 		err    error
 		listen net.Listener
 		conn   net.Conn
-		cert   tls.Certificate
 
-		nReadSzTotal, nReadCntTotal, nSendSzTotal, nSendCntTotal int = 0, 0, 0, 0
-		nReadSz, nSendSz                                         int = 0, 0
-		nIxRead, nIxWrite, nIxCount, nIxTemp, nIxMod             int = 0, 0, 0, 0, 0
-		nTemp                                                        = 0
+		nReadSzTotal, nReadCntTotal, nSendSzTotal, nSendCntTotal = 0, 0, 0, 0
+		nReadSz, nSendSz                                         = 0, 0
+		nIxRead, nIxWrite, nIxCount, nIxTemp, nIxMod             = 0, 0, 0, 0, 0
+		//nTemp                                                    = 0
 	)
 
 	// 초기화
 	pstAddress := flag.String("add", "master:50056", "ip:port")
-	pstTls := flag.String("tls", "none", "none or tls")
 	pnPackSize := flag.Int("size", 512, "packet size")
 	pnDebugMode := flag.Int("debug", 0, "debug mode - 0,1,2")
 	flag.Parse()
@@ -51,19 +47,10 @@ func main() {
 	bsBufP := make([]byte, *pnPackSize)
 
 	// 연결 대기
-	if *pstTls == "tls" {
-		cert, err = tls.LoadX509KeyPair("cert/server.crt", "cert/server.key")
-		checkErr(0, "LoadX509KeyPair", err)
+	listen, err = net.Listen("tcp", *pstAddress)
+	checkErr(0, "Listen", err)
+	fmt.Printf("Start (%s)\n", *pstAddress)
 
-		config := &tls.Config{Certificates: []tls.Certificate{cert}}
-		listen, err = tls.Listen("tcp", *pstAddress, config)
-		checkErr(0, "LoadX509KeyPair", err)
-		fmt.Printf("Start TLS (%s)\n", *pstAddress)
-	} else {
-		listen, err = net.Listen("tcp", *pstAddress)
-		checkErr(0, "Listen", err)
-		fmt.Printf("Start (%s)\n", *pstAddress)
-	}
 	defer listen.Close()
 
 	// 연결 요청 처리
@@ -77,101 +64,101 @@ func main() {
 		}
 
 		nIxCount, nIxRead, nIxWrite, nIxTemp, nIxMod = 0, 0, 0, 0, 0
-		if *pstTls == "tls" {
-		} else {
-			conn.(*net.TCPConn).SetReadBuffer(WINSIZE)
-			conn.(*net.TCPConn).SetWriteBuffer(WINSIZE)
-		}
-		nTemp = nTemp + 1
-		fmt.Printf("Accept (%v)\n", nTemp)
 
-		var syncWait sync.WaitGroup
-		syncWait.Add(1)
+		conn.(*net.TCPConn).SetReadBuffer(WINSIZE)
+		conn.(*net.TCPConn).SetWriteBuffer(WINSIZE)
 
-		go func() {
-			defer syncWait.Done()
-			// 송수신
+		//nTemp = nTemp + 1
+		//fmt.Printf("Accept (%v)\n", nTemp)
+
+		//var syncWait sync.WaitGroup
+		//syncWait.Add(1)
+		//
+		//go func() {
+		//	// 송수신
+		//	defer syncWait.Done()
+
+		for {
+			// 수신
+			nReadSz, err = conn.Read(bsBufR)
+			if err != nil {
+				if *pnDebugMode > 0 {
+					fmt.Printf("[F] Read (%v)\n", err)
+				}
+				break
+			}
+			nReadCntTotal++
+			nReadSzTotal += nReadSz
+			if *pnDebugMode == 1 {
+				fmt.Printf("Read (RSz:%d) (RCT:%d)(RST:%d)\n", nReadSz, nReadCntTotal, nReadSzTotal)
+			} else if *pnDebugMode == 2 {
+				fmt.Printf("Read (RSz:%d) (RCT:%d)(RST:%d)\n(%v)\n", nReadSz, nReadCntTotal, nReadSzTotal, bsBufR[:nReadSz])
+			}
+
+			// 원형버퍼큐 저장
+			nIxCount += nReadSz
+			if nIxCount > QUESIZE {
+				fmt.Printf("[F] IxCount (%d)\n", nIxCount)
+				break
+			}
+			nIxTemp = QUESIZE - nIxWrite
+			if nIxTemp >= nReadSz {
+				copy(bsBufQ[nIxWrite:], bsBufR[:nReadSz])
+				nIxWrite += nReadSz
+				if nIxWrite >= QUESIZE {
+					nIxWrite = 0
+				}
+			} else {
+				copy(bsBufQ[nIxWrite:], bsBufR[:nIxTemp])
+				copy(bsBufQ[:], bsBufR[nIxTemp:nReadSz])
+				nIxWrite = nReadSz - nIxTemp
+			}
+
+			// 원형버퍼큐 처리 -> 송신
 			for {
-				// 수신
-				nReadSz, err = conn.Read(bsBufR)
-				if err != nil {
+				if nIxCount < *pnPackSize {
 					if *pnDebugMode > 0 {
-						fmt.Printf("[F] Read (%v)\n", err)
+						// fmt.Printf("[W] Write IxCount(%d) < PACK_SIZE\n", nIxCount)
 					}
 					break
 				}
-				nReadCntTotal++
-				nReadSzTotal += nReadSz
-				if *pnDebugMode == 1 {
-					fmt.Printf("Read (RSz:%d) (RCT:%d)(RST:%d)\n", nReadSz, nReadCntTotal, nReadSzTotal)
-				} else if *pnDebugMode == 2 {
-					fmt.Printf("Read (RSz:%d) (RCT:%d)(RST:%d)\n(%v)\n", nReadSz, nReadCntTotal, nReadSzTotal, bsBufR[:nReadSz])
-				}
-
-				// 원형버퍼큐 저장
-				nIxCount += nReadSz
-				if nIxCount > QUESIZE {
-					fmt.Printf("[F] IxCount (%d)\n", nIxCount)
-					break
-				}
-				nIxTemp = QUESIZE - nIxWrite
-				if nIxTemp >= nReadSz {
-					copy(bsBufQ[nIxWrite:], bsBufR[:nReadSz])
-					nIxWrite += nReadSz
-					if nIxWrite >= QUESIZE {
-						nIxWrite = 0
+				nIxTemp = QUESIZE - nIxRead
+				if nIxTemp >= *pnPackSize {
+					copy(bsBufP[:], bsBufQ[nIxRead:nIxRead+*pnPackSize])
+					nIxRead += *pnPackSize
+					if nIxRead >= QUESIZE {
+						nIxRead = 0
 					}
 				} else {
-					copy(bsBufQ[nIxWrite:], bsBufR[:nIxTemp])
-					copy(bsBufQ[:], bsBufR[nIxTemp:nReadSz])
-					nIxWrite = nReadSz - nIxTemp
+					nIxMod = *pnPackSize - nIxTemp
+					copy(bsBufP[:], bsBufQ[nIxRead:])
+					copy(bsBufP[nIxTemp:], bsBufQ[:nIxMod])
+					nIxRead = nIxMod
+				}
+				nIxCount -= *pnPackSize
+				if *pnDebugMode > 0 {
+					fmt.Printf("Pack (Cnt:%d)(Wx:%d)(Ix:%d)\n", nIxCount, nIxWrite, nIxRead)
 				}
 
-				// 원형버퍼큐 처리 -> 송신
-				for {
-					if nIxCount < *pnPackSize {
-						if *pnDebugMode > 0 {
-							// fmt.Printf("[W] Write IxCount(%d) < PACK_SIZE\n", nIxCount)
-						}
-						break
-					}
-					nIxTemp = QUESIZE - nIxRead
-					if nIxTemp >= *pnPackSize {
-						copy(bsBufP[:], bsBufQ[nIxRead:nIxRead+*pnPackSize])
-						nIxRead += *pnPackSize
-						if nIxRead >= QUESIZE {
-							nIxRead = 0
-						}
-					} else {
-						nIxMod = *pnPackSize - nIxTemp
-						copy(bsBufP[:], bsBufQ[nIxRead:])
-						copy(bsBufP[nIxTemp:], bsBufQ[:nIxMod])
-						nIxRead = nIxMod
-					}
-					nIxCount -= *pnPackSize
-					if *pnDebugMode > 0 {
-						fmt.Printf("Pack (Cnt:%d)(Wx:%d)(Ix:%d)\n", nIxCount, nIxWrite, nIxRead)
-					}
-
-					// 송신 - 1 byte
-					nSendSz, err = conn.Write(bsBufP[:1])
-					if err != nil {
-						fmt.Printf("[F] Write (%v)\n", err)
-						break
-					}
-					nSendCntTotal++
-					nSendSzTotal += nSendSz
-					if *pnDebugMode == 1 {
-						fmt.Printf("Write (SSz:%d) (SCT:%d)(SST:%d)\n", nSendSz, nSendCntTotal, nSendSzTotal)
-					} else if *pnDebugMode == 2 {
-						fmt.Printf("Write (SSz:%d) (SCT:%d)(SST:%d)\n(%v)", nSendSz, nSendCntTotal, nSendSzTotal, bsBufP[:1])
-					}
+				// 송신 - 1 byte
+				nSendSz, err = conn.Write(bsBufP[:1])
+				if err != nil {
+					fmt.Printf("[F] Write (%v)\n", err)
+					break
+				}
+				nSendCntTotal++
+				nSendSzTotal += nSendSz
+				if *pnDebugMode == 1 {
+					fmt.Printf("Write (SSz:%d) (SCT:%d)(SST:%d)\n", nSendSz, nSendCntTotal, nSendSzTotal)
+				} else if *pnDebugMode == 2 {
+					fmt.Printf("Write (SSz:%d) (SCT:%d)(SST:%d)\n(%v)", nSendSz, nSendCntTotal, nSendSzTotal, bsBufP[:1])
 				}
 			}
-		}()
+		}
+		//}()
 
-		// 종료
 		conn.Close()
+		//syncWait.Wait()
 	}
 
 	// 종료
